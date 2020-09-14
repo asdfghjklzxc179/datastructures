1b450f28062c975af16e963622e00e81aba6abba
davorin <47928470+davorinpiljic@users.noreply.github.com>
2020-09-13 18:55:19 -0600

    added linked list

---
+++ singly_linked_list/singly_linked_list.py
@@ -0,0 +1,52 @@
+class Node:
+    def __init__(self, value=None, next=None):
+        self.value = value
+        self.next = next  # points to next node
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def add_to_head(self, value):
+        new_node = Node(value)
+        if self.head is None and self.tail is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+
+    def add_to_tail(self, value):
+        new_node = Node(value)
+
+        if self.head is None and self.tail is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+
+    def remove_head(self):
+        if not self.head:
+            return None
+        if self.head.next is None:
+            head_value = self.head.value
+            self.head = None
+            self.tail = None
+            return head_value
+        head_value = self.head.value
+        self.head = self.head.next
+        return head_value
+
+    def contains(self, value):
+        if self.head is None:
+            return False
+        current_node = self.head
+
+        while current_node is not None:
+            if current_node.value == value:
+                return True
+            current_node = current_node.next
+        return False
