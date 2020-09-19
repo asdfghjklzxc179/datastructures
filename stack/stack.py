96571e90eb87f3ec21a1982890459f6ab57fafd9
davorin <47928470+davorinpiljic@users.noreply.github.com>
2020-09-19 16:20:15 -0600

    stack

--- stack/stack.py
+++ stack/stack.py
@@ -10,16 +10,95 @@ return elements in Last In First Out order.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
 """
+
+
 class Stack:
     def __init__(self):
         self.size = 0
         # self.storage = ?
+        self.storage = []
 
     def __len__(self):
-        pass
+        return len(self.storage)
 
     def push(self, value):
-        pass
+        self.storage.append(value)
 
     def pop(self):
-        pass
+        if len(self.storage) == 0:
+            return None
+        return self.storage.pop()
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def add_to_tail(self, value):
+        new_node = Node(value)
+        if self.head is None and self.tail is None:
+            self.tail = new_node
+            self.head = new_node
+        else:
+            self.tail.set_next(new_node)
+            self.tail = new_node
+
+    def remove_tail(self):
+        if self.head is None and self.tail is None:
+            return None
+        if self.tail == self.head:
+            value = self.tail.get_value()
+            self.tail = None
+            self.head = None
+            return value
+        else:
+            value = self.tail.get_value()
+            current_node = self.head
+            while current_node.get_next() != self.tail:
+                current_node = current_node.get_next()
+            self.tail = current_node
+            self.tail.set_next(None)
+            return value
+
+    def add_to_head(self, value):
+        new_node = Node(value)
+
+        if self.tail is None and self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.set_next(self.head)
+            self.head = new_node
+
+    def remove_head(self):
+        if self.head is None and self.tail is None:
+            return None
+        if self.head == self.tail:
+            value = self.head.get_value()
+            self.head = None
+            self.tail = None
+            return value
+        else:
+            value = self.head.get_value()
+            self.head = self.head.get_next()
+            return value
+
+
+class StackLinkedList:
+    def __init__(self):
+        self.size = 0
+        self.storage = LinkedList()
+
+    def __len__(self):
+        return self.size
+
+    def push(self, value):
+        self.storage.add_to_head(value)
+        self.size += 1
+
+    def pop(self):
+        if self.size == 0:
+            return None
+        self.size -= 1
+        return self.storage.remove_head()
